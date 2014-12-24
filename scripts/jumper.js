133ae2c37390ceefe6a9490a00f1e87598894a84
Jeffrey Huang <jeffrey.huang977@gmail.com>
2014-12-24 18:57:07 -0500

    added moving menu

--- scripts/jumper.js
+++ scripts/jumper.js
@@ -1,11 +1,24 @@
 present = new Array();
 present[0] = 1;
+window.cacheMenuDistance = new Array();
+window.currentLocation = "Header";
 
 $(document).ready(function() {
 
 	splashLoading();
 
+	var menu = $(".menu-items").children("li");
+	menu.each(function(i) {
+		window.cacheMenuDistance[i] = {
+			offset: $(this).offset().left,
+			length: $(this).width(),
+		};
+	});
 	var sectionScrollPosition = setupJumpTo();
+	
+
+
+
 	setupParallaxForHeader(sectionScrollPosition);
 
 
@@ -16,21 +29,66 @@ $(document).ready(function() {
 function setupParallaxForHeader(offsets) {
 
 	$(window).scroll(function() {
-		if ($(window).scrollTop() + $(window).height() - $(window).height()/5 >= offsets[2] && present[0]) {
+		var scrollLength = $(window).scrollTop();	// cache the scroll offset from the top
+		var cacheMenuDistance;
+		var movingPanel = $(".active");
+
+		if (scrollLength + $(window).height() - $(window).height()/5 >= offsets[2] && present[0]) {
 			$("#uiuc-book").addClass("slideLeftToRight");
 			$("#uiuc-book").css("display", "initial");
 			present[0] = 0;
 		}
 
-		if ($(window).scrollTop() > offsets[1]) {
+		if (scrollLength <= offsets[1] - 150.0 && window.currentLocation != "Header") {
+			movingPanel.animate({
+				"margin-left": window.cacheMenuDistance[0].offset,
+				"width": window.cacheMenuDistance[0].length
+			}, 200);
+			window.currentLocation = "Header";
+		}
+
+		if (scrollLength >= offsets[1] - 150.0 && scrollLength <= offsets[2] - 150.0 && window.currentLocation != "aboutMe") {
+			movingPanel.animate({
+				"margin-left": window.cacheMenuDistance[1].offset,
+				"width": window.cacheMenuDistance[1].length
+			}, 200);
+			window.currentLocation = "aboutMe";
+		}
+
+		if (scrollLength >= offsets[2] - 150.0 && scrollLength <= offsets[3] - 150.0 && window.currentLocation != "education") {
+			movingPanel.animate({
+				"margin-left": window.cacheMenuDistance[2].offset,
+				"width": window.cacheMenuDistance[2].length
+			}, 200);
+			window.currentLocation = "education";
+		}
+
+		if (scrollLength >= offsets[3] - 150.0 && scrollLength <= offsets[4] - 150.0 && window.currentLocation != "projects") {
+			movingPanel.animate({
+				"margin-left": window.cacheMenuDistance[3].offset,
+				"width": window.cacheMenuDistance[3].length
+			}, 200);
+			window.currentLocation = "projects";
+		}
+
+		if (scrollLength >= offsets[4] - 150.0 && window.currentLocation != "contact") {
+			movingPanel.animate({
+				"margin-left": window.cacheMenuDistance[1].offset,
+				"width": window.cacheMenuDistance[1].length
+			}, 200);
+			window.currentLocation = "contact"
+		}
+
+
+		if (scrollLength > offsets[1]) {
 			return;
 		}
 
 
 		var temp = $("#main-intro");
 		var background = $("#brandingSection");
-		temp.css("margin-top", $(window).scrollTop());
-		background.css("background-position-y", -$(window).scrollTop() * .5);
+		temp.css("margin-top", scrollLength);
+		background.css("background-position-y", -scrollLength * .5);
 		temp.css("opacity", 1*Math.exp(-$(window).scrollTop() * 0.005));
 	});
 
@@ -47,6 +105,13 @@ function splashLoading() {
 }
 
 function setupJumpTo() {
+
+	var temp = $(".active");
+	temp.animate({
+				"margin-left": window.cacheMenuDistance[0].offset,
+				"width": window.cacheMenuDistance[0].length
+			}, 0);
+
 
 	var buttons = $(".navi-bar").find("ul").children("li");
 	var sectionScrollPosition = new Array();

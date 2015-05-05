cfd3be9f20b4144202ee986017f31902d6d20a86
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-05-05 12:55:33 -0500

    Major update

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,35 +1,105 @@
-$(document).ready(function() {
+$(document).ready(main);
 
-	var heightOfPage = $(window).height();
+function main()
+{
+    var boxes = $(".box");
+    $(boxes).each(function() {
+        var children = $(this).children();
+        var left = $(children).eq(0), right = $(children).eq(1);
+        $(left).height($(left).height() > $(right).height() ? $(left).height() : $(right).height());
+        $(right).height($(left).height() > $(right).height() ? $(left).height() : $(right).height());
+    });
+    
+    var workInfo = parseWorkInformation();
+    
 
-	var menuItems = $(".menu-item");
+    var otherInfo = parseOtherInformation();
+    
+    
+    $('.other-injection-point').html(otherInfo);
+    $('.work-injection-point').html(workInfo);
 
-	for (var i = 0; i < menuItems.length; i++) {
+}
 
-		menuItems[i].onclick = function() {
-			var idOf = this.id;
-			console.log(idOf);
-			switch (idOf) {
-				case "aboutButton":
-					$('html, body').animate({
-				        scrollTop: 0
-				    }, 1000);
-					break;
-				case "wordButton":
-					$('html, body').animate({
-				        scrollTop: $("#personalprojects").offset().top
-				    }, 1000);
-					break;
-				case "samplesButton":
-					$('html, body').animate({
-				        scrollTop: $("#codesamples").offset().top
-				    }, 1000);
-					break;
-				default: return;
-			}
+function parseWorkInformation()
+{
+    var work_info = $('.work-information');
+    var html = "";
+    
+    for (var i = 0; i < work_info.length; i++)
+    {
+        html += parseWorkBlurb(work_info[i]);
+    }
+    
+    $(work_info).each(function() { $(this).remove(); });
+    
+    return html;
+}
 
-		};
+function parseWorkBlurb(info)
+{
+    var name = $(info).attr('data-name');
+    var title = $(info).attr('data-title');
+    var start = $(info).attr('data-start');
+    var end = $(info).attr('data-end');
+    var png = $(info).attr('data-png');
+    var brag = $(info).html();
+    
+    var html = "<li class='work-information-parsed row'>";
+    html    +=      "<p class='col-md-9 work-info'>";
+    html    +=          "<span class='work-info-employer'>" + name + "</span>";
+    html    +=          "<br/>";
+    html    +=          "<span class='work-info-title'>" + title + "</span>";
+    html    +=          "<br/>";
+    html    +=          "<span class='work-info-time'>" + start + " ~ " + end + "</span>";
+    html    +=          "<br/><br/>";
+    html    +=          "<span class='work-info-brag'>" + brag + "</span>";
+    html    +=      "</p>";
+    html    +=      "<img src='" + 'images/' + png + "' class='work-info-png col-md-3' \>";
+    html    += "</li>"
+    
+    return html;
+    
+}
 
-	}
+function parseOtherInformation()
+{
+    var other = $('.other-information');
+    var html = "";
+    
+    for (var i = 0; i < other.length; i++)
+    {
+        html += parseOtherBlurb(other[i]);
+    }
+    
+    $(other).each(function() { $(this).remove(); });
+    
+    return html;
+}
 
-});
+function parseOtherBlurb(info)
+{
+    var name = $(info).attr('data-name');
+    var title = $(info).attr('data-title');
+    var done = $(info).attr('data-done');
+    var partners = $(info).attr('data-partners');
+    var png = $(info).attr('data-png');
+    var brag = $(info).html();
+    
+    var html = "<li class='other-information-parsed row'>";
+    html    +=      "<p class='col-md-9 other-info'>";
+    html    +=          "<span class='other-info-name'>" + name + "</span>";
+    html    +=          "<br/>";
+    html    +=          "<span class='other-info-title'>" + title + "</span>";
+    html    +=          "<br/>";    
+    html    +=          "<span class='other-info-teams'>" + partners + "</span>";
+    html    +=          "<br/>";
+    html    +=          "<span class='other-info-day'>" + done + "</span>";
+    html    +=          "<br/><br/>";
+    html    +=          "<span class='other-info-brag'>" + brag + "</span>";
+    html    +=      "</p>";
+    html    +=      "<img src='" + 'images/' + png + "' class='other-info-png col-md-3' \>";
+    html    += "</li>"
+    
+    return html;
+}

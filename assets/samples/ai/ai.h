87148653b4c5b91f36085f8e3b37a375a5093615
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-08-12 10:15:44 -0700

    Added pulse site

--- assets/samples/ai/ai.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "game.h"
-
-void find_food(board * cur_board, int * distance_map, int row, int col,
-               int distance, int * closest_distance);
-void ai_move(game * game);
